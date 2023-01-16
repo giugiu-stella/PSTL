2a201ebd95c943b793b0c9867d48a36cf8be596d
Lynadjdjdjd <114083371+Lynadjdjdjd@users.noreply.github.com>
2023-01-16 23:25:25 +0100

    petite modification

--- parie1.js
+++ parie1.js
@@ -12,21 +12,29 @@ const fs = require('fs')
 var logger = fs.createWriteStream('Output.txt', {
     flags: 'a' // 'a'  (old data will be preserved)
 })
+var valeur = true 
+console.log("faire une partie") ;
+ var i =0 ;
+while ( valeur ==true &&  i<22) //22 parties  22*3 =66 bits  
+{          for (let j = 0 ; j<3 ; j++)  // un joueur a le droit a 3 lancers de dés 
+          {
+                var valeur_string = bit_aleatoir().toString()+ bit_aleatoir().toString() +bit_aleatoir().toString();  
 
+                var index = codage.indexOf(valeur_string,0);
+                
+                if (index==6)  { var valeur_dé= '1'; }
+                else 
+                {  if (index==7)   {var valeur_dé= '2';}
+                                    else  {
+                                        var valeur_dé= (index+1).toString() ;
+                                        }
+                    
+                }
+                logger.write(valeur_dé);
+                logger.write('\n'); 
+            }
 
-for (let i = 0 ; i<22 ; i++)
-{  
-   var valeur_string = bit_aleatoir().toString()+ bit_aleatoir().toString() +bit_aleatoir().toString();
-   var index = codage.indexOf(valeur_string,0);
-   console.log(valeur_string,index)
-   if (index==6)  { var valeur_dé= '1'; }
-   else 
-   {  if (index==7)   {var valeur_dé= '2';}
-                    else  {
-                        var valeur_dé= (index+1).toString() ;
-                         }
-      
-   }
-   logger.write(valeur_dé);
-   logger.write('\n');
+    i=i+3 ;
+   console.log("faire une partie?") ;
 }
+
