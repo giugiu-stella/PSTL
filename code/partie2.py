a02d1720282860107bf94f36f12cff882dccb92f
Lynadjdjdjd <114083371+Lynadjdjdjd@users.noreply.github.com>
2023-02-06 23:53:27 +0100

    modif sur la construction des couples

--- code/partie2.py
+++ code/partie2.py
@@ -17,57 +17,58 @@ liste_x1  =[x1]
 #nb_bit(x0)=33==> x0+1bit de x1     
 
 #calcule de x0 
+sauvgarder=""
 for cpt in range(11):
     line = file1.readline() 
     if cpt !=10:
+        
         if len(binaire(line))==1:
+            sauvgarder=binaire(line)[0][0:2]
             for i in  range (len(liste_x0)): 
                 liste_x0 [i]=binaire(line)[0]+liste_x0[i]
         
         if len(binaire(line))==2 :   # si la liste [000,001]  alors on devra rajouter dans chaque element le premier et le 2eme 
+            sauvgarder=binaire(line)[0][0:2]
             for j in  range (len(liste_x0)): 
                     liste_x0.append(binaire(line)[1]+liste_x0[j])
                     liste_x0 [j]=binaire(line)[0]+liste_x0[j]
     else : #juste 2 bit le 3 eme c'est un bit de x1 
+        sauvgarder=(binaire(line)[0][0:1])
+        
         if len(binaire(line))==1:
+            
             for i in  range (len(liste_x0)): 
                 liste_x0 [i]=(binaire(line)[0])[1:3]+liste_x0[i]
         
-        if len(binaire(line))==2 :    
+        if len(binaire(line))==2 : 
             for j in  range (len(liste_x0)): 
                     liste_x0.append((binaire(line)[1])[1:3]+liste_x0[j])
                     liste_x0 [j]=(binaire(line)[0])[1:3]+liste_x0[j]
 
 
 #Calcule de x1
-for cpt in range(11,23):
-    line = file1.readline() 
-    if cpt==11 : #uniquement 1 bit 
-        if len(binaire(line))==1:
-            for i in  range (len(liste_x1)): 
-                liste_x1 [i]=binaire(line)[0][0:1]+liste_x1[i]
-        if len(binaire(line))==2 : 
-            for j in  range (len(liste_x1)): 
-                    liste_x1.append(binaire(line)[1][0:1]+liste_x1[j])
-                    liste_x1 [j]=binaire(line)[0][0:1]+liste_x1[j]
-    else :
-        if cpt==22:#uniquement 1 bit daonc on aura 2 bits inutiles 
-            if len(binaire(line))==1:
-                for i in  range (len(liste_x1)): 
-                    liste_x1 [i]=binaire(line)[0][2:3]+liste_x1[i]
-            if len(binaire(line))==2 : 
-                for j in  range (len(liste_x1)): 
-                        liste_x1.append(binaire(line)[1][2:3]+liste_x1[j])
-                        liste_x1 [j]=binaire(line)[0][2:3]+liste_x1[j]
-        else : 
-            if len(binaire(line))==1:
-                for i in  range (len(liste_x1)): 
-                    liste_x1 [i]=binaire(line)[0]+liste_x1[i]
-            if len(binaire(line))==2 : 
-                for j in  range (len(liste_x1)): 
-                        liste_x1.append(binaire(line)[1]+liste_x1[j])
-                        liste_x1 [j]=binaire(line)[0]+liste_x1[j]
+for cpt in range(11):
+            line = file1.readline()  
+            if cpt==10:
+                 if len(binaire(line))==1:
+                    for i in  range (len(liste_x1)): 
+                        liste_x1 [i]=binaire(line)[0][2:3]+liste_x1[i]
+                 if len(binaire(line))==2 : 
+                    for j in  range (len(liste_x1)): 
+                            liste_x1.append(binaire(line)[1][2:3]+liste_x1[j])
+                            liste_x1 [j]=binaire(line)[0][2:3]+liste_x1[j]
+            else :
+                if len(binaire(line))==1:
+                    for i in  range (len(liste_x1)): 
+                        liste_x1 [i]=binaire(line)[0]+liste_x1[i]
+                if len(binaire(line))==2 : 
+                    for j in  range (len(liste_x1)): 
+                            liste_x1.append(binaire(line)[1]+liste_x1[j])
+                            liste_x1 [j]=binaire(line)[0]+liste_x1[j]
 
+for j in range (len (liste_x1)):
+     liste_x1 [j]=liste_x1[j]+sauvgarder
+     
 #construction des couples 
 couple =[]
 for x0 in liste_x0:
