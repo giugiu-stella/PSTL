138eb7665ae64622bd48f49d673a034738642bfa
giugiu <61977659+giugiu-stella@users.noreply.github.com>
2023-03-27 22:33:25 +0200

    modification fonction rejet

--- code/avec_reget/construction.py
+++ code/avec_reget/construction.py
@@ -1,136 +1,72 @@
+def aux(chaine,i,couple):
+    max=len(chaine)
+    chaine_110=chaine[0:max-(i+1)]+"110"+chaine[max-i::]
+    chaine_111=chaine[0:max-(i+1)]+"111"+chaine[max-i::]
+    taille=len(chaine_110)
+
+    x0_110=chaine_110[taille-32:taille]
+    x1_110=chaine_110[taille-64:taille-32]
+
+    x0_111=chaine_111[taille-32:taille]
+    x1_111=chaine_111[taille-64:taille-32]
+    couple.append((x0_110,x1_110))
+    couple.append((x0_111,x1_111))
+    return couple
+
 def liste_couple_rejet(listecouple):
     couple =[]
-    rejet0="110"
-    rejet1="111"
-    #18 psk les 3 derniers bits de x0 sans oublier
-    for i in range(0, 63, 3):
+    for i in range(0, 64, 3):
         for (xo,x1) in listecouple:
-            chaine=xo+x1
-            if(i==0):
-                newchaine0=rejet0+chaine
-                newchaine1=rejet1+chaine
-            else:
-                newchaine0=chaine[0:i]+rejet0+chaine[i::]
-                newchaine1=chaine[0:i]+rejet1+chaine[i::]
-
-            newx0_0=newchaine0[0:31]
-            newx0_1=newchaine1[0:31]
-
-            newx1_0=newchaine0[31:63]
-            newx1_1=newchaine1[31:63]
-            couple.append((newx0_0,newx1_0))
-            couple.append((newx0_1,newx1_1))
-
+            chaine=x1+xo
+            aux(chaine,i,couple)
     return couple
 
 def liste_couple_rejet_deux(listecouples):
     couple=[]
-    rejet0="110"
-    rejet1="111"
-
-    for k in range(0, 63, 3):
-        for i in range(0, 63-(k*2), 3):     # il y a 2^2 cas (2 rejets et chaque rejet a 2 possibilité)
+    for k in range(0, 64, 3):
+        for i in range(k+3, 64, 3):     # il y a 2^2 cas (2 rejets et chaque rejet a 2 possibilité)
             for (xo,x1) in listecouples:    # chaque cas ci dessous effectue 2 sous-cas donc on a bien 2 * 2 
-                chaine=xo+x1
-                fixe_0=chaine[0:k]+"110"+chaine[0:i]
-                newchaine0_0=fixe_0+rejet0+chaine[i::]
-                newchaine1_0=fixe_0+rejet1+chaine[i::]
-
-                newx0_0_0=newchaine0_0[0:31]
-                newx0_1_0=newchaine1_0[0:31]
-
-                newx1_0_0=newchaine0_0[31:63]
-                newx1_1_0=newchaine1_0[31:63]
-                couple.append((newx0_0_0,newx1_0_0))
-                couple.append((newx0_1_0,newx1_1_0))
-
-                fixe_1=chaine[0:k]+"111"+chaine[0:i]
-                newchaine0_1=fixe_1+rejet0+chaine[i::]
-                newchaine1_1=fixe_1+rejet1+chaine[i::]
-
-                newx0_0_1=newchaine0_1[0:31]
-                newx0_1_1=newchaine1_1[0:31]
-
-                newx1_0_1=newchaine0_1[31:63]
-                newx1_1_1=newchaine1_1[31:63]
-                couple.append((newx0_0_1,newx1_0_1))
-                couple.append((newx0_1_1,newx1_1_1))
-        
+                chaine=x1+xo
+                fixe=chaine[0:64-k]+"110"+chaine[64-k::]
+                aux(fixe,i,couple)
+                fixe=chaine[0:64-k]+"111"+chaine[64-k::]
+                aux(fixe,i,couple)
     return couple 
 
 def liste_couple_rejet_trois(listecouple):
     couple=[]
-    rejet0="110"
-    rejet1="111"
-
-    for v in range(0, 63, 3):
-        for k in range(v, 63-(3), 3):
-            for i in range(k, 63-(3*3-1), 3):   # il y a 8 cas possibles => 2^3 (3 rejets qui ont deux possibilités)
+    for v in range(0, 64, 3):
+        for k in range(v+3, 64, 3):
+            for i in range(k+3, 64, 3):   # il y a 8 cas possibles => 2^3 (3 rejets qui ont deux possibilités)
                 for (xo,x1) in listecouple:     # chaque cas ci dessous effectue 2 sous-cas donc on a bien 4 * 2   
                     chaine=xo+x1
-
-                    #cas 0
-                    fixe_0=chaine[0:v]+"110"+chaine[v:k]+"111"+chaine[k:i]
-                    newchaine0_0=fixe_0+rejet0+chaine[i::]
-                    newchaine1_0=fixe_0+rejet1+chaine[i::]
-
-                    newx0_0_0=newchaine0_0[0:31]
-                    newx0_1_0=newchaine1_0[0:31]
-
-                    newx1_0_0=newchaine0_0[31:63]
-                    newx1_1_0=newchaine1_0[31:63]
-                    couple.append((newx0_0_0,newx1_0_0))
-                    couple.append((newx0_1_0,newx1_1_0))
-
                     #cas 1
-                    fixe_1=chaine[0:v]+"111"+chaine[v:k]+"111"+chaine[k:i]
-                    newchaine0_1=fixe_1+chaine[0:i]+rejet0+chaine[i::]
-                    newchaine1_1=fixe_1+chaine[0:i]+rejet1+chaine[i::]
-
-                    newx0_0_1=newchaine0_1[0:31]
-                    newx0_1_1=newchaine1_1[0:31]
-
-                    newx1_0_1=newchaine0_1[31:63]
-                    newx1_1_1=newchaine1_1[31:63]
-                    couple.append((newx0_0_1,newx1_0_1))
-                    couple.append((newx0_1_1,newx1_1_1))
+                    fixe=chaine[0:i]+"___"+chaine[i:64-v]+"___"+chaine[64-v::]
+                    aux(fixe,i,couple)
 
                     #cas 2
-                    fixe_2=chaine[0:v]+"110"+chaine[v:k]+"111"+chaine[k:i]
-                    newchaine0_2=fixe_2+chaine[0:i]+rejet0+chaine[i::]
-                    newchaine1_2=fixe_2+chaine[0:i]+rejet1+chaine[i::]
+                    fixe=chaine[0:i]+"..."+chaine[i:64-v]+"..."+chaine[64-v::]
+                    aux(fixe,i,couple)
 
-                    newx0_0_2=newchaine0_2[0:31]
-                    newx0_1_2=newchaine1_2[0:31]
-
-                    newx1_0_2=newchaine0_2[31:63]
-                    newx1_1_2=newchaine1_2[31:63]
-                    couple.append((newx0_0_2,newx1_0_2))
-                    couple.append((newx0_1_2,newx1_1_2))
-                    
                     #cas 3
-                    fixe_3=chaine[0:v]+"111"+chaine[v:k]+"110"+chaine[k:i]
-                    newchaine0_3=fixe_3+chaine[0:i]+rejet0+chaine[i::]
-                    newchaine1_3=fixe_3+chaine[0:i]+rejet1+chaine[i::]
-
-                    newx0_0_3=newchaine0_3[0:31]
-                    newx0_1_3=newchaine1_3[0:31]
-
-                    newx1_0_3=newchaine0_3[31:63]
-                    newx1_1_3=newchaine1_3[31:63]
-                    couple.append((newx0_0_3,newx1_0_3))
-                    couple.append((newx0_1_3,newx1_1_3))
+                    fixe=chaine[0:i]+"___"+chaine[i:64-v]+"..."+chaine[64-v::]
+                    aux(fixe,i,couple)
+                    
+                    #cas 4
+                    fixe=chaine[0:i]+"..."+chaine[i:64-v]+"___"+chaine[64-v::]
+                    aux(fixe,i,couple)
 
     return couple
+
 
 # utilisation de _ _ _ et . . . pour le visuel
 # ___ => 110
 # ... => 111
-# 18 => 63
-# 9 => 31
+# 18 => 64
+# 9 => 32
 #test
-couple=[("123456789","abcdefghi")]
-couplenew=liste_couple_rejet_trois(couple)
+#couple=[("123456789","abcdefghi")]
+#couplenew=liste_couple_rejet_deux(couple)
 #for i in couplenew:
   #  print(i)
 
