94bf97ac48dfa5ac2c0c4bcbf8d6adb524b32c77
giugiu <61977659+giugiu-stella@users.noreply.github.com>
2023-04-02 23:12:35 +0200

    j ai trouvé pour rejet !!!

--- code/avec_reget/construction.py
+++ code/avec_reget/construction.py
@@ -1,16 +1,27 @@
+def check(c,couple):
+
+    for i in couple :
+        if(i==c):
+            return False
+    return True
+
 def aux(chaine,i,couple):
     max=len(chaine)
-    chaine_110=chaine[0:max-(i+1)]+"110"+chaine[max-i::]
-    chaine_111=chaine[0:max-(i+1)]+"111"+chaine[max-i::]
+    chaine_110=chaine[0:max-i]+"_"+chaine[max-i::]
+    chaine_111=chaine[0:max-i]+"."+chaine[max-i::]
     taille=len(chaine_110)
 
-    x0_110=chaine_110[taille-32:taille]
-    x1_110=chaine_110[taille-64:taille-32]
+    x0_110=chaine_110[taille-4:taille]
+    x1_110=chaine_110[taille-8:taille-4]
 
-    x0_111=chaine_111[taille-32:taille]
-    x1_111=chaine_111[taille-64:taille-32]
-    couple.append((x0_110,x1_110))
-    couple.append((x0_111,x1_111))
+    x0_111=chaine_111[taille-4:taille]
+    x1_111=chaine_111[taille-8:taille-4]
+    c1=(x0_110,x1_110)
+    c2=(x0_111,x1_111)
+    if(check(c1,couple)):
+        couple.append((x0_110,x1_110))
+    if(check(c2,couple)):
+        couple.append((x0_111,x1_111))
     return couple
 
 def liste_couple_rejet(listecouple):
@@ -27,9 +38,9 @@ def liste_couple_rejet_deux(listecouples):
         for i in range(k+3, 64, 3):     # il y a 2^2 cas (2 rejets et chaque rejet a 2 possibilité)
             for (xo,x1) in listecouples:    # chaque cas ci dessous effectue 2 sous-cas donc on a bien 2 * 2 
                 chaine=x1+xo
-                fixe=chaine[0:64-k]+"110"+chaine[64-k::]
+                fixe=chaine[0:64-k]+"___"+chaine[64-k::]
                 aux(fixe,i,couple)
-                fixe=chaine[0:64-k]+"111"+chaine[64-k::]
+                fixe=chaine[0:64-k]+"..."+chaine[64-k::]
                 aux(fixe,i,couple)
     return couple 
 
@@ -58,6 +69,20 @@ def liste_couple_rejet_trois(listecouple):
 
     return couple
 
+def liste_couple_rejet_n(rejets,listecouple):
+    if rejets == 0:
+        return listecouple
+   
+    else:
+        couples = []
+        listecouplenew=liste_couple_rejet_n(rejets-1,listecouple)
+        for i in range(0,64):
+            for (xo, x1) in listecouplenew:
+                chaine=x1+xo
+                aux(chaine,i,couples)
+        
+        return couples
+
 
 # utilisation de _ _ _ et . . . pour le visuel
 # ___ => 110
@@ -65,11 +90,14 @@ def liste_couple_rejet_trois(listecouple):
 # 18 => 64
 # 9 => 32
 #test
-#couple=[("123456789","abcdefghi")]
-#couplenew=liste_couple_rejet_deux(couple)
-#for i in couplenew:
-  #  print(i)
+couple=[("1234","abcd")]
+couplenew=liste_couple_rejet_n(10,couple)
 
-            
+fichier = open("essay.txt", "a")
+for (x0 ,x1 ) in couplenew:
+        couple.append((x0,x1) )
+        fichier.write("( "+str(x0) + " , "+ str(x1)+ " )")
+        fichier.write("\n")
+
 
     
